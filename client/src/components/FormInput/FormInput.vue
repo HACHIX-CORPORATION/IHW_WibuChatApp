f7ff17babe7fb25c3cffa805e788e8133f436a6d
duy.ntHachix <107833418+duyhachix@users.noreply.github.com>
2022-08-02 14:07:58 +0700

    #13 Fix delete no used array

--- client/src/components/FormInput/FormInput.vue
+++ client/src/components/FormInput/FormInput.vue
@@ -36,46 +36,7 @@ export default {
   },
   data() {
     return {
-     
       inputContent: "",
-      images: [
-        {
-          id: 1,
-          url: "02.jpg",
-        },
-        {
-          id: 2,
-          url: "akame.jpg",
-        },
-        {
-          id: 3,
-          url: "aqua.jpg",
-        },
-        {
-          id: 4,
-          url: "asuna.jpg",
-        },
-        {
-          id: 5,
-          url: "dark_saber.jpg",
-        },
-        {
-          id: 6,
-          url: "hanezawa.jpg",
-        },
-        {
-          id: 7,
-          url: "maisakurajima.jpg",
-        },
-        {
-          id: 8,
-          url: "natusme2.jpg",
-        },
-        {
-          id: 9,
-          url: "rem.jpg",
-        },
-      ],
     };
   },
 
