2fed4136cb42f8644ad4c3dde5295eec103d40f7
thanhduyhaxhi-x <duy.nguyen@hachi-x.com>
2022-08-13 13:43:23 +0700

    ✨ #40 Add login logical (axios)

--- client/src/screen/LoginPage/LoginPage.js
+++ client/src/screen/LoginPage/LoginPage.js
@@ -1,30 +1,76 @@
 import FomrInput from "../../components/FormInput/FormInput.vue";
 import Constant from "@/constant/Constant"; // import file contain variables
+import axios from "axios";
+
+const baseUrl = "http://127.0.0.1:5000";
+
+axios.defaults.headers = {
+    "Cache-Control": "no-cache",
+    Pragma: "no-cache",
+    Expires: "0",
+};
 
 export default {
-	name: "LoginPage",
-	components: {
-		FomrInput,
-	},
-	data() {
-		return {
-			routes: [{ name: "Lobby", url: "/lobby" }],
-			inputInfos: [Constant.REGISTER_INFOS[0], Constant.REGISTER_INFOS[1]],
-		};
-	},
+    name: "LoginPage",
+    components: {
+        FomrInput,
+    },
+    data() {
+        return {
+            routes: [{ name: "Lobby", url: "/lobby" }],
+            inputInfos: [Constant.REGISTER_INFOS[0], Constant.REGISTER_INFOS[1]],
+            userName: "",
+            passWord: "",
+        };
+    },
+    computed: {
+        // enable login button when input field is not empty
+        isDisable() {
+            return !this.userName || !this.passWord;
+        },
+    },
+    methods: {
+        onShow() {
+            // if type = text -> convert type = password and vice_versa
+            if (this.inputInfos[1].type == "text") {
+                this.inputInfos[1].type = "password";
+            } else {
+                this.inputInfos[1].type = "text";
+            }
+        },
+        async onLogin() {
+            try {
+                let response = await axios.post(baseUrl + "/login", {
+                    username: this.userName,
+                    password: this.passWord,
+                });
+                console.log({ response });
+                // check status from response axios
+                if (response.status === 200) {
+                    alert("Login succesfully");
+                    this.$router.push({ name: "Lobby" });
+                }
+                // console.log(response);
+            } catch (error) {
+                alert("Login failed !");
+                console.log(error);
+            }
+        },
 
-	methods: {
-		onShow() {
-			// if type = text -> convert type = password and vice_versa
-			if (this.inputInfos[1].type == "text") {
-				this.inputInfos[1].type = "password";
-			} else {
-				this.inputInfos[1].type = "text";
-			}
-		},
-		onLogin() {
-			this.$router.push({ name: "Lobby" });
-		},
-		computed: {},
-	},
+        /**
+         *
+         * @param {Object} inputValue is a object contain index, input content
+         */
+        // lay ra value sau khi nhap vao moi o input
+        inputSubmit(inputValue) {
+            console.log(inputValue);
+            if (inputValue.index === 0) {
+                this.userName = inputValue.content;
+            }
+            if (inputValue.index === 1) {
+                this.passWord = inputValue.content;
+            }
+            console.log({ username: this.userName, password: this.passWord });
+        },
+    },
 };
--- client/src/screen/LoginPage/LoginPage.vue
+++ client/src/screen/LoginPage/LoginPage.vue
@@ -5,20 +5,16 @@
       <form>
         <div class="login__form-infos">
           <div class="login__form-info">
-            <FomrInput
-              v-for="(input, index) in inputInfos"
-              :info="input.info"
-              :title="input.title"
-              :icon="input.icon"
-              :type="input.type"
-              :key="index"
-            />
+            <FomrInput v-for="(input, index) in inputInfos" :key="index" :info="input.info" :title="input.title"
+              :icon="input.icon" :type="input.type" :inputIndex="index" @onInput="inputSubmit" />
             <fa id="fas" icon="eye" @click="onShow" />
-            <button class="submit--button" @click="onLogin()">Login</button>
+            <button class="submit--button" :disabled="isDisable" @click.prevent="onLogin">
+              Login
+            </button>
           </div>
           <div class="login__footer">
             <!-- 1 way to use router-link to change page without having href -->
-            <router-link :to="'/signup'">You don't have account ??</router-link>
+            <router-link :to="'/register'">You don't have account ??</router-link>
             <a href="">Forgot password</a>
           </div>
         </div>
@@ -31,5 +27,4 @@
 </script>
 
 <style scoped src="./LoginPage.css">
-
 </style>
