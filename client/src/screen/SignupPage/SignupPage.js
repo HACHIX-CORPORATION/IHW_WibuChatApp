1abd396d452584083bfaeb58057d1a52d5179b6a
thanhduyhaxhi-x <duy.nguyen@hachi-x.com>
2022-08-14 09:40:04 +0700

    👍 #42 Add register logical
    add axios

--- client/src/screen/SignupPage/SignupPage.js
+++ client/src/screen/SignupPage/SignupPage.js
@@ -1,73 +1,135 @@
 import FormInput from "@/components/FormInput/FormInput.vue";
 import ImageSelector from "@/components/ImageSelector/ImageSelector.vue";
 import Constant from "@/constant/Constant";
+import axios from "axios";
+
+const baseUrl = "http://127.0.0.1:5000";
+axios.defaults.headers = {
+    "Cache-Control": "no-cache",
+    Pragma: "no-cache",
+    Expires: "0",
+};
 
 export default {
-	name: "SignupPage",
-	components: {
-		FormInput,
-		ImageSelector,
-	},
+    name: "SignupPage",
+    components: {
+        FormInput,
+        ImageSelector,
+    },
 
-	data() {
-		return {
-			caption: "Choose your waifu",
-			inputInfos: Constant.REGISTER_INFOS,
-			invalid: true,
-			imagePreview: "",
-			images: Constant.AVATAR_IMAGES,
-		};
-	},
-	methods: {
-		onChangeInput(data) {
-			this.inputInfos[data.index].info = data.content;
-		},
-		displaySubmit() {
-			this.invalid = !this.invalid;
-		},
-		onSubmit() // check all input field'value is empty
-		{
-			if (this.inputInfos[0].info == "") {
-				alert("Please insert username");
-			} else if (this.inputInfos[1].info == "") {
-				alert("Please insert password");
-			} else if (this.inputInfos[2].info == "") {
-				alert("Please insert re-password");
-			} else if (this.inputInfos[3].info == "") {
-				alert("Please insert telephone");
-			} else if (this.inputInfos[4].info == "") {
-				alert("Please insert mail");
-			} else if (this.inputInfos[5].info == "") {
-				alert("Please insert date of birth");
-			}
-			// If not empty, then check validation of every field
-			else {
-				if (
-					this.inputInfos[0].info.length < 4 ||
-					this.inputInfos[0].info.length > 16
-				) {
-					alert("Username length must contain 4-16 characters");
-				} else if (
-					this.inputInfos[1].info.length < 8 ||
-					this.inputInfos[1].info.length > 16 ||
-					this.inputInfos[1].info === this.inputInfos[1].info.toLowerCase() ||
-					this.inputInfos[1].info ===
-						this.inputInfos[1].info.replace(/[^\w ]/, "")
-				) {
-					alert("Password invalid");
-				} else if (this.inputInfos[2].info !== this.inputInfos[1].info) {
-					alert("Confirm password must match with password");
-				} else if (
-					!this.inputInfos[4].info.match(
-						/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-					)
-				) {
-					alert("Mail invalid");
-				} else {
-					alert("Register successfully !");
-					this.$router.push("login");
-				}
-			}
-		},
-	},
+    data() {
+        return {
+            routes: [{ name: "Login", url: "/login" }],
+            caption: "Choose your waifu",
+            inputInfos: Constant.REGISTER_INFOS,
+            invalid: true,
+            imagePreview: "",
+            images: Constant.AVATAR_IMAGES,
+            userName: "",
+            passWord: "",
+            rePassword: "",
+            telephoneNum: "",
+            mailAdd: "",
+            dobInfo: "",
+        };
+    },
+    methods: {
+        displaySubmit() {
+            this.invalid = !this.invalid;
+        },
+        async onSubmit() {
+            // check all input field'value is empty
+            if (this.inputInfos[0].info == "") {
+                alert("Please insert username");
+            } else if (this.inputInfos[1].info == "") {
+                alert("Please insert password");
+            } else if (this.inputInfos[2].info == "") {
+                alert("Please insert re-password");
+            } else if (this.inputInfos[3].info == "") {
+                alert("Please insert telephone");
+            } else if (this.inputInfos[4].info == "") {
+                alert("Please insert mail");
+            } else if (this.inputInfos[5].info == "") {
+                alert("Please insert date of birth");
+            }
+            // If not empty, then check validation of every field
+            else {
+                if (
+                    this.inputInfos[0].info.length < 4 ||
+                    this.inputInfos[0].info.length > 16
+                ) {
+                    alert("Username length must contain 4-16 characters");
+                } else if (
+                    this.inputInfos[1].info.length < 8 ||
+                    this.inputInfos[1].info.length > 16 ||
+                    this.inputInfos[1].info === this.inputInfos[1].info.toLowerCase() ||
+                    this.inputInfos[1].info ===
+                    this.inputInfos[1].info.replace(/[^\w ]/, "")
+                ) {
+                    alert("Password invalid");
+                } else if (this.inputInfos[2].info !== this.inputInfos[1].info) {
+                    alert("Confirm password must match with password");
+                } else if (!this.inputInfos[4].info.match(
+                        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                    )) {
+                    alert("Mail invalid");
+                } else {
+                    try {
+                        let response = await axios.post(baseUrl + "/register", {
+                            username: this.userName,
+                            password: this.passWord,
+                            rePassword: this.rePassword,
+                            telephone: this.telephoneNum,
+                            mail: this.mailAdd,
+                            date: this.dobInfo,
+                        });
+                        console.log(response);
+                        if (response.status === 200) {
+                            alert("Register successfully !");
+                            this.$router.push("login");
+                        } else {
+                            throw new Error("register failed");
+                        }
+                    } catch (error) {
+                        if (error.response.status === 400) {
+                            alert("This username already exited, try new one");
+                        } else {
+                            alert("Register failed !!!!");
+                        }
+                        console.log(error);
+                    }
+                }
+            }
+        },
+        inputSubmit(inputValue) {
+            if (inputValue.index === 0) {
+                this.userName = inputValue.content;
+            }
+            if (inputValue.index === 1) {
+                this.passWord = inputValue.content;
+            }
+            if (inputValue.index === 2) {
+                this.rePassword = inputValue.content;
+            }
+            if (inputValue.index === 3) {
+                this.telephoneNum = inputValue.content;
+            }
+            if (inputValue.index === 4) {
+                this.mailAdd = inputValue.content;
+            }
+            if (inputValue.index === 5) {
+                this.dobInfo = inputValue.content;
+            }
+            this.inputInfos[inputValue.index].info = inputValue.content;
+            // console.log(inputValue);
+            console.log({
+                username: this.userName,
+                password: this.passWord,
+                repassword: this.rePassword,
+                telephone: this.telephoneNum,
+                mail: this.mailAdd,
+                date: this.dobInfo,
+            });
+        },
+    },
 };
--- client/src/screen/SignupPage/SignupPage.vue
+++ client/src/screen/SignupPage/SignupPage.vue
@@ -7,16 +7,8 @@
           <div class="register__info--blocks">
             <div class="register__infocontainer">
               <div class="register__infocontainer--block1">
-                <FormInput
-                  v-for="(input, index) in inputInfos"
-                  :info="input.info"
-                  :title="input.title"
-                  :icon="input.icon"
-                  :type="input.type"
-                  :key="index"
-                  :inputIndex="index"
-                  @on-input="onChangeInput"
-                />
+                <FormInput v-for="(input, index) in inputInfos" :key="index" :info="input.info" :title="input.title"
+                  :icon="input.icon" :type="input.type" :inputIndex="index" @onInput="inputSubmit" />
               </div>
               <div class="register__infocontainer--block2">
                 <ImageSelector :caption="caption" />
@@ -29,11 +21,7 @@
                 this site.
               </p>
             </div>
-            <button
-              class="submit--button"
-              :disabled="invalid"
-              @click="onSubmit"
-            >
+            <button class="submit--button" :disabled="invalid" @click="onSubmit">
               Submit
             </button>
           </div>
@@ -41,9 +29,7 @@
       </div>
       <div class="register__footer">
         <!-- 1 way to use router-link to change page without having href -->
-        <router-link id="router" :to="'/login'"
-          >Already have an account ?</router-link
-        >
+        <router-link id="router" :to="'/login'">Already have an account ?</router-link>
         <p>
           By registering, you agree to Wichat's
           <span>Term of services</span> and <span>Privacy Policy</span>
