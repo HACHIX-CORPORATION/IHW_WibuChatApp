feca0497fba7ca7f49f868c492ac5f8d31a9286d
unknown <duy.nguyen@hachi-x.com>
2022-08-22 17:28:38 +0700

    👍  #63 Change variable of client matching server
    This branch got formatting issue

--- client/src/service/API/api.service.js
+++ client/src/service/API/api.service.js
@@ -2,20 +2,20 @@ import axios from 'axios';
 const baseUrl = 'http://127.0.0.1:5000';
 
 axios.defaults.headers = {
-	'Cache-Control': 'no-cache',
-	Pragma: 'no-cache',
-	Expires: '0',
+    'Cache-Control': 'no-cache',
+    Pragma: 'no-cache',
+    Expires: '0',
 };
 
 const ApiService = {
-	addNewRoom: function (newName) {
-		// POST request always return response, here is "axios.post..."
-		return axios.post(baseUrl + '/room', {
-			roomName: newName,
-		});
-	},
-	getAllRoom: function () {
-		return axios.get(baseUrl + '/rooms');
-	},
+    addNewRoom: function(newName) {
+        // POST request always return response, here is "axios.post..."
+        return axios.post(baseUrl + '/api/room/', {
+            room_name: newName,
+        });
+    },
+    getAllRoom: function() {
+        return axios.get(baseUrl + '/api/room/');
+    },
 };
 export default ApiService;
