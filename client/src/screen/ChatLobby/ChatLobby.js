b8153381c3dc92564d3166cd293134b19a8b3edb
duy.ntHachix <107833418+duyhachix@users.noreply.github.com>
2022-08-22 09:38:10 +0700

    👍 #59 fix format file

--- client/src/screen/ChatLobby/ChatLobby.js
+++ client/src/screen/ChatLobby/ChatLobby.js
@@ -2,9 +2,9 @@ import BoxChat from '../../components/BoxChat/BoxChat.vue';
 import SideBar from '../../components/SideBar/SideBar.vue';
 
 export default {
-    name: 'ChatLobby',
-    components: {
-        SideBar,
-        BoxChat,
-    },
+	name: 'ChatLobby',
+	components: {
+		SideBar,
+		BoxChat,
+	},
 };
