db3faf274b07eca96c422d1412ac77365ac37400
MinhTuanHachi-x <nguyen.minhtuan@hachi-x.com>
2022-08-24 10:22:58 +0700

    👍 #74 add Fix review

--- server/sockets/connection_socket.py
+++ server/sockets/connection_socket.py
@@ -8,32 +8,37 @@ from app.app import app
 socketio = SocketIO(app, cors_allowed_origins='*')
 
 @socketio.on('message')
-def chat_room(data):
+def on_message(data):
     mess = data['mess']
-    room = data['room']
+    room_name = data['room_name']
     user_id = data['user_id']
-    room_db = RoomModel.find_by_name(room)
+    room_db = RoomModel.find_by_name(room_name)
     user = UserModel.find_by_id(user_id)
-    if room_db and user:
+
+    if mess == "":
+        print('message is null')
+    elif room_db and user:
         message = MessageModel(room_id=room_db.room_id, user_id=user.user_id, message=mess)
         message.save_to_db()
-        emit("new_message", user.username + " : " + mess, room=room)
+        emit("new_message", mess , room=room_name)
+        print("Message is : {}".format(str(mess)))
     else :
+        print('failed message')
         send('failed message')
-
-
+    
 @socketio.on('join')
 def on_join(data):
-    room = data['room']
+    room_name = data['room_name']
     user_id = data['user_id']
-    join_room(room)
-    room_db = RoomModel.find_by_name(room)
+    join_room(room_name)
+    room_db = RoomModel.find_by_name(room_name)
     user = UserModel.find_by_id(user_id)
     if room_db and user:
-        emit("new_join", user.username + " has joined the " + room + " room.", room=room)
+        emit("new_join", user.username + " has joined the " + room_name + " room.", room=room_name)
+        print("join successfull")
     else:
+        print("Not found room")
         send('Not found room')
-
 
 @socketio.on('disconnect')
 def test_disconnect():
