d84bad2d11cb69ea636c17952fd527aa99262779
duy.ntHachix <107833418+duyhachix@users.noreply.github.com>
2022-08-24 10:33:07 +0700

    ✨ #69 send message

--- client/src/components/BoxChat/BoxChat.js
+++ client/src/components/BoxChat/BoxChat.js
@@ -1,4 +1,4 @@
-import socketClient from "@/service/Socket";
+import socketClient from '@/service/Socket';
 
 export default {
 	name: 'BoxChat',
@@ -7,21 +7,33 @@ export default {
 			appName: 'Wichat',
 			newText: '',
 			messengerList: [],
+			roomName: '',
 		};
 	},
 	methods: {
 		sendMess() {
-			let userId = localStorage.getItem('userId')
+			let userId = localStorage.getItem('userId');
 			socketClient.emit('chat_room', {
-				mess: newText,
-				room: ,
+				mess: this.newText,
+				room: this.roomName,
 				userID: userId,
-			})
+			});
+			console.log({
+				mess: this.newText,
+				room: this.roomName,
+				userID: userId,
+			});
 			// check noi dung co phai la empty hay khong thi moi push
 			if (this.newText != '') {
 				this.messengerList.push(this.newText);
 				this.newText = '';
 			} else this.newText = '';
 		},
+	},
+	created() {
+		this.emitter.on('on-transfer-room-name', (currentRoomName) => {
+			console.log('Room: ', { roomName: currentRoomName }, ' joined');
+			this.roomName = currentRoomName;
+		});
 	},
 };
